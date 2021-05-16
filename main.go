c0c34dd6df121fa07511c3ccd8c3b6c3011f604d
James Scholes <james@jamesscholes.com>
2021-05-16 14:11:49 -0500

    Switch to using a template rather than a static file to serve index page

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -17,8 +18,20 @@ func main() {
 		port = "8080"
 	}
 
-	fs := http.FileServer(http.FS(staticFS))
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFS(staticFS, "index.html")
+		if err != nil {
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			log.Printf("Error loading template index.html: %w", err)
+			return
+		}
+
+		if err := t.Execute(w, nil); err != nil {
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			log.Printf("Error executing template index.html: %w", err)
+			return
+		}
+	})
 
 	log.Printf("Serving task creation interface on port %s; press Ctrl+C to quit", port)
 
